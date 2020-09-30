ca69ae918d621048128f34f1aa63da44c735a260
Ivana Kostic <kosticivana92@gmail.com>
2020-09-30 13:43:50 +0200

    wip/LoginPage edit

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -8,28 +8,32 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasicPage {
 
-	public LoginPage(WebDriver driver, WebDriverWait wait,JavascriptExecutor js) {
+	public LoginPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super(driver, wait, js);
-		
+
 	}
-	
+
 	public void loginButton() {
 		WebElement loginbtn = driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div[2]/div[2]/ul/li[2]/a"));
 		loginbtn.click();
 	}
-	
+
 	public WebElement getUsername() {
 		return this.driver.findElement(By.name("username"));
 	}
+
 	public WebElement getPassword() {
 		return this.driver.findElement(By.name("password"));
 	}
+
 	public WebElement rememberMeCheckBox() {
 		return this.driver.findElement(By.name("remember_me"));
 	}
+
 	public WebElement loginBtn() {
 		return this.driver.findElement(By.name("btn_submit"));
 	}
+
 	public void loginForm(String username, String password) {
 		this.getUsername().clear();
 		this.getUsername().sendKeys(username);
@@ -37,7 +41,7 @@ public class LoginPage extends BasicPage {
 		this.getPassword().sendKeys(password);
 		rememberMeCheckBox().click();
 		loginBtn().click();
-		
+
 	}
-	
+
 }
