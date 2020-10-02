c5d81bfc716ffb3113907b37814a695afb07c091
Ivana Kostic <kosticivana92@gmail.com>
2020-10-02 18:20:29 +0200

    ProfileTest created

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -9,10 +9,13 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import pages.AuthPage;
+import pages.CartSummaryPage;
 import pages.LocationPopUpPage;
 import pages.LoginPage;
+import pages.MealItemPage;
 import pages.NotificationSystemPage;
 import pages.ProfilePage;
+import pages.SearchResultPage;
 
 public class ProfileTest extends BasicTest {
 
@@ -25,6 +28,9 @@ public class ProfileTest extends BasicTest {
 		LoginPage lp = new LoginPage(this.driver, this.wait, this.js);
 		ProfilePage pp = new ProfilePage(this.driver, this.wait, this.js);
 		AuthPage ap = new AuthPage(this.driver, this.wait, this.js);
+		MealItemPage mp = new MealItemPage(this.driver, this.wait, this.js);
+		CartSummaryPage csp = new CartSummaryPage(this.driver, this.wait, this.js);
+		SearchResultPage srp = new SearchResultPage(this.driver, this.wait, this.js);
 
 		SoftAssert sa = new SoftAssert();
 
@@ -37,7 +43,7 @@ public class ProfileTest extends BasicTest {
 
 		this.driver.navigate().to(this.baseUrl + "member/profile");
 
-		pp.inputAll("Ivana", "Kostic", "user@gmail.com", "Nis", "18000", "09090", "Serbia", "Serbia", "Nis");
+		pp.inputAll("Michael", "Moore", "user@gmail.com", "Nis", "18000", "09090", "Serbia", "Serbia", "Nis");
 
 		Assert.assertTrue(nsp.getSuccessfulMessage().equals("Setup Successful"));
 
@@ -62,8 +68,10 @@ public class ProfileTest extends BasicTest {
 		lpp.closeLocationHeader();
 		
 		lp.loginForm(this.userId, this.userPass);
+		
+		Thread.sleep(2000);
 
-		Assert.assertTrue(nsp.getSuccessfulMessage().equals("Login Successfull"));
+		sa.assertTrue(nsp.getSuccessfulMessage().equals("Login Successfull"));
 
 		this.driver.navigate().to(this.baseUrl + "member/profile");
 		
@@ -71,20 +79,21 @@ public class ProfileTest extends BasicTest {
 		
 		pp.uploadPhotoFile(imgPath);
 		
-		Assert.assertTrue(nsp.getSuccessfulMessage().equals("Profile Image Uploaded Successfully"));
+		sa.assertTrue(nsp.getSuccessfulMessage().equals("Profile Image Uploaded Successfully"));
 		
 		wait.until(ExpectedConditions.invisibilityOf(nsp.getSuccessfulMessage()));
 		
 		pp.removePhoto();
 		
-		Assert.assertTrue(nsp.getSuccessfulMessage().equals("Profile Image Deleted Successfully"));
+		sa.assertTrue(nsp.getSuccessfulMessage().equals("Profile Image Deleted Successfully"));
 		
 		wait.until(ExpectedConditions.invisibilityOf(nsp.getSuccessfulMessage()));
 		
 		ap.logout();
 
-		Assert.assertTrue(nsp.getSuccessfulMessage().equals("Logout Successfull!"));
+		sa.assertTrue(nsp.getSuccessfulMessage().equals("Logout Successfull!"));
 
+		sa.assertAll();
 	}
 
 }
