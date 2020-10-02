2aed309ba04d6df207f2e27880a55add42dd54d1
Ivana Kostic <kosticivana92@gmail.com>
2020-10-02 18:41:29 +0200

    SearchResultPage edit:

--- src/pages/SearchResultPage.java
+++ src/pages/SearchResultPage.java
@@ -1,5 +1,8 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +18,22 @@ public class SearchResultPage extends BasicPage {
 	public WebElement qtyInput() {
 		return this.driver.findElement(By.name("product_qty"));
 	}
-	public WebElement addToCart() {
-		return this.driver.findElement(By.xpath("//*[@id='body']/section[1]/div/div/div[2]/div/div[3]/div[2]/a"));
+	
+
+	
+	public List<WebElement> getSearchResult(){
+		return this.driver.findElements(By.xpath("//*[@class='product-name']/a"));
+		
+	}
+	
+	public ArrayList<String> nameOfAllMeals(){
+		ArrayList<String> names = new ArrayList<>();
+		for (int i = 0; i <this.getSearchResult().size(); i++) {
+			names.add(this.getSearchResult().get(i).getText());
+		}
+		return names;
+	}
+	public int numberOfSearchResults() {
+		return this.getSearchResult().size();
 	}
 }
