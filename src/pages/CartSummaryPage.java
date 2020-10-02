5a6932b86e3c8b328a745b979191dbfe61b51336
Ivana Kostic <kosticivana92@gmail.com>
2020-10-02 19:07:52 +0200

    CartSummaryPage edit

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CartSummaryPage extends BasicPage {
@@ -9,6 +11,15 @@ public class CartSummaryPage extends BasicPage {
 	public CartSummaryPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super(driver, wait, js);
 		
+		//*[@id="cartSummary"]/div/div[1]/a[2]
 	}
 
+	public WebElement clearAllButton() {
+		return this.driver.findElement(By.xpath("//*[@id=\"cartSummary\"]/div/div[1]/a[2]"));
+		
+	}
+	
+	public void clearbtn() {
+		clearAllButton().click();
+	}
 }
