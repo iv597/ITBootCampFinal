f379d01eb62c3d92540ef27f0f7eba10803debd1
Ivana Kostic <kosticivana92@gmail.com>
2020-10-02 18:37:04 +0200

    MealItemPage edit

--- src/pages/MealItemPage.java
+++ src/pages/MealItemPage.java
@@ -1,5 +1,24 @@
 package pages;
 
-public class MealItemPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MealItemPage extends BasicPage {
+
+	public MealItemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+		
+	}
+	public WebElement addToCart() {
+		return this.driver.findElement(By.xpath("//*[@id='body']/section[1]/div/div/div[2]/div/div[3]/div[2]/a"));
+	}
+	public void addMeal(String qty) {
+		this.wait.until(ExpectedConditions.elementToBeClickable(addToCart()));
+		
+	}
 
 }
